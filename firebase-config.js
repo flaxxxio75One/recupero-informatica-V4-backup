9df9fc17a260396135568948b84277a994eed4d9
flaxxxio75One <flaxxxio75@gmail.com>
2026-06-13 16:13:37 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,12 @@
-// FIREBASE CONFIG
-// 1. Crea un progetto su https://console.firebase.google.com
-// 2. Aggiungi una Web App
-// 3. Copia qui la configurazione firebaseConfig
-// 4. Sostituisci enabled: false con enabled: true
-
 const FIREBASE_SETTINGS = {
-  enabled: false,
+  enabled: true,
   studentId: "arianna",
   firebaseConfig: {
-    apiKey: "INCOLLA_QUI",
-    authDomain: "INCOLLA_QUI",
-    projectId: "INCOLLA_QUI",
-    storageBucket: "INCOLLA_QUI",
-    messagingSenderId: "INCOLLA_QUI",
-    appId: "INCOLLA_QUI"
-  }
+  apiKey: "AIzaSyBUewe14CBDjPo3UT0jZWJFpDTm4uAkIHY",
+  authDomain: "arianna-esame-informatica.firebaseapp.com",
+  projectId: "arianna-esame-informatica",
+  storageBucket: "arianna-esame-informatica.firebasestorage.app",
+  messagingSenderId: "604298305132",
+  appId: "1:604298305132:web:80e0531f3f9c905929c249"
+ }
 };
